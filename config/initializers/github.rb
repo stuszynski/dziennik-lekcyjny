3f4c56b5c37ddb6341463a63d6ae9ee6058199c5
Wlodek Bzyl <matwb@ug.edu.pl>
2012-01-30 20:34:03 +0100

    Poprawki w widokach. Dodałem css3-social-signin-buttons.

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -2,6 +2,10 @@ raw_config = File.read("#{ENV['HOME']}/.credentials/services.yml")
 
 github = YAML.load(raw_config)['github']
 
+# puts "-"*80
+# puts "#{github}"
+# puts "-"*80
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, github['key'], github['secret']
 end
