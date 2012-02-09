0fa572334760a821f4bd35679a92aaf4b3cdfb12
Wlodek Bzyl <matwb@ug.edu.pl>
2012-02-10 00:08:44 +0100

    Prepare fo production.

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,13 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+require 'rack/cache'
+
 module DziennikLekcyjny
   class Application < Rails::Application
+
+    config.middleware.delete Rack::Cache
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
